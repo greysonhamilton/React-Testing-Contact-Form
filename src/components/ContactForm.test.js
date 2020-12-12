e0c5dcb95d351fdbed1d319b3c00a14134d5d176
Greyson <greyson.r.hamilton@gmail.com>
2020-12-12 14:48:07 -0500

    mvp and validation test

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {act, fireEvent, render, screen, waitFor} from '@testing-library/react';
+import {act, fireEvent, render, screen} from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('Renders ContactForm without errors', () => {
@@ -10,7 +10,7 @@ test('Renders ContactForm without errors', () => {
 
 // Note that first name does not have an id and only allows a maximum of three characters
 
-test('User can fill out the form', () => {
+test('User can fill out the form', async() => {
 
     render(<ContactForm />);
 
@@ -18,30 +18,43 @@ test('User can fill out the form', () => {
     const lnameInput = screen.getByPlaceholderText(/Burke/i);
     const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
     const msgInput = screen.getByLabelText(/message/i);
-    const button = screen.getByRole('button', {name: /submit/i});
 
-    act(async() => {
     fireEvent.change(fnameInput, {target: {value: 'Gry'}});
     fireEvent.change(lnameInput, {target: {value: 'Hamilton'}});
     fireEvent.change(emailInput, {target: {value: 'greyson.r.hamilton@gmail.com'}});
     fireEvent.change(msgInput, {target: {value: 'I come from the land down under!'}});
-
-    });
-
+    
+    const button = screen.getByRole('button', {name: /submit/i});
     fireEvent.click(button);
 
-    // await waitFor(() => expect(gry).toBeInTheDocument());
+    const gry = await screen.findByText(/gry/i);
+    expect(gry).toBeInTheDocument();
 
-    // const gry = await screen.findByText(/gry/i);
-    // expect(gry).toBeInTheDocument();
+    const hamilton = await screen.findByText(/hamilton/i);
+    expect(hamilton).toBeInTheDocument();
 
-    // const hamilton = await screen.findByText(/hamilton/i);
-    // expect(hamilton).toBeInTheDocument();
+    const greyson = await screen.findByText(/greyson.r.hamilton@gmail.com/i);
+    expect(greyson).toBeInTheDocument();
 
-    // const greyson = await screen.findByText(/greyson.r.hamilton@gmail.com/i);
-    // expect(greyson).toBeInTheDocument();
-
-    // const down = await screen.findByText(/i come from the land down under/i);
-    // expect(down).toBeInTheDocument();
+    const down = await screen.findByText(/i come from the land down under/i);
+    expect(down).toBeInTheDocument();
 
 });
+
+test('testing max length requirment of First Name input', () => {
+
+    render(<ContactForm />);
+
+    const fnameInput = screen.getByPlaceholderText(/Edd/i);
+    fireEvent.change('Grey');
+
+});
+
+test('testing email input validation', () => {
+
+    render(<ContactForm />);
+
+    const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+    fireEvent.change('mybox#box.com');
+    
+})
